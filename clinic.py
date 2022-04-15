07fc5272f5cd8e585629a778d55e396a2a078d2d
Edward Sproat <82000483+sproat17@users.noreply.github.com>
2022-04-15 14:36:24 -0500

    Add files via upload

--- clinic.py
+++ clinic.py
@@ -17,7 +17,7 @@ cursor = db.cursor()
 #sg.change_look_and_feel('BrownBlue')
 sg.change_look_and_feel('DarkTeal9')
 
-def doctor_form_window():
+def receptionist_form():
     sg.change_look_and_feel('LightBrown9')
     layout = [
         [sg.Text("Enter Employee Portal")]
@@ -27,12 +27,58 @@ def doctor_form_window():
     print("yeys")
     window.close()
     # All the stuff inside your window. This is the PSG magic code compactor...
-    layout = [  [sg.Text('Some text on Row 1')],
-        [sg.Text('Enter something on Row 2'), sg.InputText()],
-        [sg.OK(), sg.Cancel()]]
+    layout = [  [sg.Text('Dr blah blah blah examining blah blah')],
+        [sg.Text("Diagnosis Code"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')],
+        [sg.Text("End Time"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')],
+        [sg.Text("Doctor Notes"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')]
+    ]
 
     # Create the Window
-    window = sg.Window('Window Title', layout)
+    window = sg.Window('Doctor_Form', layout, size=(500,600))
+    # Event Loop to process "events"
+    while True:             
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Cancel'):
+            break
+
+    window.close()
+def doctor_form_window():
+    
+    # All the stuff inside your window. This is the PSG magic code compactor...
+    layout = [  [sg.Text('Dr blah blah blah examining blah blah')],
+        [sg.Text("Diagnosis Code"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')],
+        [sg.Text("End Time"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')],
+        [sg.Text("Doctor Notes"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')]
+    ]
+
+    # Create the Window
+    window = sg.Window('Doctor_Form', layout, size=(500,600))
+    # Event Loop to process "events"
+    while True:             
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Cancel'):
+            break
+
+    window.close()
+    
+def doctor_schedule():
+    sg.change_look_and_feel('LightBrown5')
+    layout = [
+        [sg.Text("Enter Employee Portal")]
+    ]
+
+    window = sg.Window('Doctor Evaluation Form', layout, modal=True)
+    print("yeys")
+    window.close()
+    # All the stuff inside your window. This is the PSG magic code compactor...
+    layout = [  [sg.Text('Dr blah blah blah examining blah blah')],
+        [sg.Text("Diagnosis Code"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')],
+        [sg.Text("End Time"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')],
+        [sg.Text("Doctor Notes"), sg.Input(key='EID'), sg.Text(size=(40,1), key='-OUTPUT-')]
+    ]
+
+    # Create the Window
+    window = sg.Window('Doctor_Form', layout, size=(500,600))
     # Event Loop to process "events"
     while True:             
         event, values = window.read()
@@ -43,15 +89,22 @@ def doctor_form_window():
 
 
 employee_column = [
-    [sg.Text("Enter Employee Portal")],
+    [sg.Text("Login As Doctor")],
     [sg.Input(key='EID')],
+    [sg.Text(size=(40,1), key='-OUTPUT-')],
+    [sg.Text("Login as Nurse")],
+    [sg.Input(key='NID')],
+    [sg.Text(size=(40,1), key='-OUTPUT-')],
+    [sg.Text("Login as Receptionist")],
+    [sg.Input(key='RID')],
     [sg.Text(size=(40,1), key='-OUTPUT-')]
 ]
 
 patient_column = [
     [sg.Text("Enter Patient Portal")],
     [sg.Input(key='PID')],
-    [sg.Text(size=(40,1), key='-OUTPUT-')]
+    [sg.Text(size=(40,1), key='-OUTPUT-')],
+    [sg.Button('Ok'), sg.Button('Quit')]
 ]
 
 # Define the window's contents
@@ -61,7 +114,6 @@ layout = [
         sg.VSeperator(),
         sg.Column(patient_column)
     ],
-    [sg.Button('Ok'), sg.Button('Quit')]
 ]
 
 # Create the window
@@ -74,11 +126,11 @@ while True:
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
     # Output a message to the window
-    employee_query = "select fname, lname from DOCTOR WHERE employeeID=" + str(values['EID']) + ";"
+    employee_query = "select fname, lname from RECEPTIONIST WHERE employeeID=" + str(values['RID']) + ";"
     cursor.execute(employee_query)
     myResult = cursor.fetchall()
     print(myResult)
-    window['-OUTPUT-'].update('Welcome Dr ' + str(myResult))
+    window['-OUTPUT-'].update('Welcome ' + str(myResult))
     doctor_form_window()
 
 
